c1dd73352837b01952ab4ea62aca4931708e433c
DavidePrelati <davide.prelati@gmail.com>
2024-04-09 16:50:49 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const savePerson = () => {
+    const name = document.getElementById("nameInput").value.trim();
+    const surname = document.getElementById("surnameInput").value.trim();
+    if (name !== "" && surname !== "") {
+        let person = JSON.parse(localStorage.getItem("savedPerson")) || [];
+        person.push({name, surname});
+        localStorage.setItem("savedPerson", JSON.stringify(person));
+        displayPerson();
+    } else {
+        alert("Inserisci un nome e un cognome validi.");
+    }
+};
+
+const displayPerson = () => {
+    const savedPerson = JSON.parse(localStorage.getItem("savedPerson")) || [];
+    const savedPersonDiv = document.getElementById("savedPerson");
+    savedPersonDiv.innerHTML = "";
+    savedPerson.forEach(entry => {
+        const entryElement = document.createElement("div");
+        entryElement.textContent = `Nome: ${entry.name}, Cognome: ${entry.surname}`;
+        savedPersonDiv.appendChild(entryElement);
+    });
+};
+const removePerson = () => {
+    let person = JSON.parse(localStorage.getItem("savedPerson")) || [];
+    if (person.length > 0) {
+        person.pop();
+        localStorage.setItem("savedPerson", JSON.stringify(person));
+        displayPerson();
+    } else {
+        alert("Non ci sono dati da rimuovere.");
+    }
+};
+
+
+window.onload = () => {
+    displayPerson();
+};
+
+document.getElementById("saveButton").addEventListener("click", savePerson);
+document.getElementById("removeButton").addEventListener("click", removePerson);
